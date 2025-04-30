1a3244775a6bd934011d2d57d83586f2fa80ee21
Fernando Dodino <fernando.dodino@gmail.com>
2025-04-29 21:36:48 -0300

    Formatting and using node 22

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,7 +1,7 @@
 import {Alumno, Parcial, cabulero, cambiarCriterioEstudio, estudia, estudioso, hijoDelRigor}  from './app'
 
 const parcialPdep = Parcial("Paradigmas de programacion", 5)
-const parcialOerativos = Parcial("Sistemas operativos", 2)
+const parcialOperativos = Parcial("Sistemas operativos", 2)
 const nico = Alumno("Nico",[10, 3, 1993],124124,["sysop", "proyecto"],estudioso)
 
 describe('Criterios de estudio', () => {
@@ -14,7 +14,7 @@ describe('Criterios de estudio', () => {
   })
 
   test('Cabulero no estudia si el nombre de la materia no cumple con su condición', () => {
-    expect(cabulero(parcialOerativos)).toBeFalsy()
+    expect(cabulero(parcialOperativos)).toBeFalsy()
   })
 
   test('Hijo del rigor estudia si cumple la condición el parcial', () => {
@@ -22,21 +22,21 @@ describe('Criterios de estudio', () => {
   })
 
   test('Hijo del rigor no estudia si no cumple la condición el parcial', () => {
-    expect(hijoDelRigor(4)(parcialOerativos)).toBeFalsy()
+    expect(hijoDelRigor(4)(parcialOperativos)).toBeFalsy()
   })
 })
 
 describe('Alumnos', () => {
 
   test('Alumno cambia criterio de estudio', () => {
-    expect(nico.criterioEstudio(parcialOerativos)).toBeTruthy()
+    expect(nico.criterioEstudio(parcialOperativos)).toBeTruthy()
     const nuevoNico = cambiarCriterioEstudio (hijoDelRigor(4), nico)
-    expect(nuevoNico.criterioEstudio(parcialOerativos)).toBeFalsy()
+    expect(nuevoNico.criterioEstudio(parcialOperativos)).toBeFalsy()
   })
 
   test('Saber si un alumno estudia para un parcial', () => {
     const nuevoNico = cambiarCriterioEstudio (hijoDelRigor(4), nico)
-    expect(estudia(parcialOerativos, nuevoNico)).toBeFalsy()
+    expect(estudia(parcialOperativos, nuevoNico)).toBeFalsy()
   })
 
 })
--- src/app.ts
+++ src/app.ts
@@ -1,45 +1,61 @@
-type Fecha = [number,number,number] // Tuplas en Typescript
+
+/************************************************************************************************/
+/*                                          Parcial                                            **/
+/************************************************************************************************/
 
 type Parcial = {
-  materia : string,
-  cantidadDePreguntas :number
+  materia: string,
+  cantidadDePreguntas: number
 }
 
-const Parcial = (_materia:string,_preguntas:number):Parcial => {
+const Parcial = (_materia: string, _preguntas: number): Parcial => {
   return {
     materia: _materia,
     cantidadDePreguntas: _preguntas
   } as const
 }
 
-type CriterioEstudio = (_parcial :Parcial) => boolean
+/************************************************************************************************/
+/*                                  Criterios de estudio                                       **/
+/************************************************************************************************/
+type CriterioEstudio = (_parcial: Parcial) => boolean
+
+const estudioso: CriterioEstudio = (_) => true
+
+const hijoDelRigor = (preguntas: number): CriterioEstudio => (_parcial: Parcial) => _parcial.cantidadDePreguntas > preguntas
+
+const cabulero: CriterioEstudio = (_parcial: Parcial) => _parcial.materia.length % 2 === 0
+
+const cambiarCriterioEstudio = (nuevoCriterio: CriterioEstudio, alumno: Alumno): Alumno => { return { ...alumno, criterioEstudio: nuevoCriterio } as const }
+
+
+/************************************************************************************************/
+/*                                            Alumno                                           **/
+/************************************************************************************************/
+type Fecha = [number, number, number] // Tuplas en Typescript
 
 type Alumno = {
-  nombre : string,
-  fechaNacimiento :Fecha,
+  nombre: string,
+  fechaNacimiento: Fecha,
   legajo: number,
-  materiasQueCursa : string[],
-  criterioEstudio : CriterioEstudio
+  materiasQueCursa: string[],
+  criterioEstudio: CriterioEstudio
 }
 
-const Alumno = (_nombre: string,_fechaNacimiento: Fecha,_legajo :number,_materiasQueCursa:string[],_criterio:CriterioEstudio) : Alumno => 
-{ return {
+const Alumno = (_nombre: string, _fechaNacimiento: Fecha, _legajo: number, _materiasQueCursa: string[], _criterio: CriterioEstudio): Alumno => {
+  return {
     nombre: _nombre,
     fechaNacimiento: _fechaNacimiento,
     legajo: _legajo,
     materiasQueCursa: _materiasQueCursa,
     criterioEstudio: _criterio
- }  as const }
- 
-const estudioso :CriterioEstudio = (_) => true
+  } as const
+}
 
-const hijoDelRigor = (preguntas: number) :CriterioEstudio => (_parcial:Parcial) => _parcial.cantidadDePreguntas > preguntas 
+/************************************************************************************************/
+/*                                    Casos de uso                                             **/
+/************************************************************************************************/
 
-const cabulero :CriterioEstudio = (_parcial :Parcial) => _parcial.materia.length % 2 === 0
+const estudia = (parcial: Parcial, alumno: Alumno): boolean => alumno.criterioEstudio(parcial)
 
-const cambiarCriterioEstudio  = (nuevoCriterio: CriterioEstudio,  alumno: Alumno) :Alumno => {return {...alumno, criterioEstudio: nuevoCriterio } as const}
-
-const estudia = (parcial:Parcial, alumno:Alumno): boolean => alumno.criterioEstudio(parcial)
-
-
-export {Parcial, Alumno,estudioso,hijoDelRigor,cabulero,cambiarCriterioEstudio, estudia}
+export { Parcial, Alumno, estudioso, hijoDelRigor, cabulero, cambiarCriterioEstudio, estudia }
